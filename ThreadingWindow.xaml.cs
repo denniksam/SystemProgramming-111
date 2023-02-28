8d182f18d70fdc9855ee0a2013a7c453877fa1aa
Denys Samoilenko <denniksam@gmail.com>
2023-02-28 19:38:25 +0200

    Threading -- 2

--- ThreadingWindow.xaml.cs
+++ ThreadingWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace SystemProgramming_111
         {
             InitializeComponent();
         }
-
+        #region part 1 - "зависание интерфейса"
         private void ButtonStart1_Click(object sender, RoutedEventArgs e)
         {
             Start1();
@@ -44,7 +44,9 @@ namespace SystemProgramming_111
                 Thread.Sleep(300);
             }
         }
+        #endregion
 
+        #region part 2 - Исключение (обращение к другому потоку)
         private void ButtonStart2_Click(object sender, RoutedEventArgs e)
         {
             new Thread( Start1 ).Start();  //  - исключение (изменения из другого потока)
@@ -54,20 +56,23 @@ namespace SystemProgramming_111
         {
 
         }
-        
+        #endregion
 
+        #region part 3 - Решение
         private void ButtonStart3_Click(object sender, RoutedEventArgs e)
         {
+            isStopped = false;
             new Thread( Start3 ).Start();
         }
 
         private void ButtonStop3_Click(object sender, RoutedEventArgs e)
         {
-
+            isStopped = true;
         }
+        private bool isStopped;  // "общая" переменная, доступная всем потокам
         private void Start3()
         {
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 10 && !isStopped; i++)
             {
                 this.Dispatcher.Invoke(() =>  // делегирование работы потоку окна
                 {
@@ -78,5 +83,59 @@ namespace SystemProgramming_111
                 Thread.Sleep(300);
             }
         }
+        #endregion
+
+        #region part 4 - Взаимодействие потоков (продолжение прогресса)
+        private void ButtonStart4_Click(object sender, RoutedEventArgs e)
+        {
+            isStopped4 = false;
+            // параметр для Start4 передается в вызов .Start(0)
+            new Thread( Start4 ).Start(savedIndex4);
+            if(savedIndex4 == 0)
+            {
+                ConsoleBlock.Text = "";
+            }
+        }
+        private void ButtonStop4_Click(object sender, RoutedEventArgs e)
+        {
+            isStopped4 = true;
+        }
+        private bool isStopped4;
+        private int savedIndex4;  // при прерывании потока - храним последний i
+        private void Start4(object? startIndex)
+        {
+            // при приеме параметра обязательно преобразовать его к
+            // ожидаемому типу (и проверить на валидность)
+            if(startIndex is int startFrom)
+            {
+                for (int i = startFrom; i < 10; i++)
+                {
+                    if(isStopped4)
+                    {
+                        savedIndex4 = i;
+                        return;
+                    }
+                    this.Dispatcher.Invoke(() =>  // делегирование работы потоку окна
+                    {
+                        progressBar4.Value = (i + 1) * 10;
+                        ConsoleBlock.Text += i.ToString() + "\n";
+                    });
+
+                    Thread.Sleep(300);
+                }
+                // если поток закончился (без прерываний) 
+                savedIndex4 = 0;
+            }
+        }
+        #endregion
+        /* Д.З. Реализовать блокироку возможности запустить новый поток
+         * пока не закончит работу предыдущий (в т.ч. прерыванием)
+         * а) disable кнопки
+         *   либо
+         * б) сохранять ссылку на поток:  thread = new Thread(...)
+         *    и проверять ее на null
+         * Если поток прерывается (не закончившись) менять надпись
+         * кнопки Start на Resume. По окончанию - Start
+         */
     }
 }
