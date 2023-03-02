8df9ecad35b007755886bbd0176ad68ad8365f5e
Denys Samoilenko <denniksam@gmail.com>
2023-03-02 19:35:41 +0200

    CancellationToken

--- MultiWindow.xaml.cs
+++ MultiWindow.xaml.cs
@@ -64,29 +64,40 @@ namespace SystemProgramming_111
         #endregion
 
         #region variant 2
+        private CancellationTokenSource cts;   // Источник токенов отмены потоков
         private void ButtonStart2_Click(object sender, RoutedEventArgs e)
         {
             sum2 = 100;
             progressBar2.Value = 0;
+            cts = new();
             for (int i = 0; i < 12; i++)
             {
-                new Thread(plusPercent2).Start();
+                new Thread(plusPercent2).Start(cts.Token);
             }
         }
 
         private void ButtonStop2_Click(object sender, RoutedEventArgs e)
         {
-
+            cts?.Cancel();
         }
 
         private double sum2;
         private readonly object locker2 = new();     // объект для синхронизации
 
-        private void plusPercent2()
+        private void plusPercent2(object? token)
         {
+            if (token is not CancellationToken) return;
+
+            CancellationToken cancellationToken = (CancellationToken) token;
+            // отмена токена (cts?.Cancel()) сама по себе не остановит поток, а только
+            // установит токен в отмененное состояние. Поток должен проверять токен
+            // в разрешенных для отмены местах и принимать решение об отмене.     
+
             double val;
             lock (locker2)                           // синхро-блок
             {                                        // поток, который первым входит в
+                if (cancellationToken.IsCancellationRequested) return;
+                // cancellationToken.ThrowIfCancellationRequested();
                 val = sum2;                          // блок, закрывает locker2 и открывает
                 Thread.Sleep(random.Next(250, 350)); // его по выходу из блока.
                 double percent = 10;                 // Другие потоки, дойдя до lock
@@ -101,5 +112,63 @@ namespace SystemProgramming_111
             });
         }
         #endregion
+
+
+        #region variant 3
+        private void ButtonStart3_Click(object sender, RoutedEventArgs e)
+        {
+            sum3 = 100;
+            progressBar3.Value = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                new Thread(plusPercent3).Start(i+1);
+            }
+        }
+
+        private void ButtonStop3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private double sum3;
+        private readonly object locker3 = new();     // объект для синхронизации
+
+        private void plusPercent3( object? month )
+        {
+            if (month is not int) return;
+
+            double val;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(random.Next(250, 350));   // часть рассчетов, 
+                // место для возможной отмены потока
+            }
+            
+            double percent = 10 + (int)month;      // вынесенная
+            double factor = 1 + percent / 100;     // за синхроблок
+
+            lock (locker3)                           
+            {                                      // внутри блока
+                val = sum3;                        // остается часть рассчетов
+                val *= factor;                     // которую нельзя более
+                sum3 = val;                        // разделять
+            }                                        
+
+            Dispatcher.Invoke(() =>
+            {
+                ConsoleBlock.Text += month + " " + percent + " " + val + "\n";
+                progressBar3.Value += 100.0 / 12;
+            });
+        }
+        #endregion
     }
+
+    class ThreadData3   // комплексный тип для передачи нескольких данных в поток
+    {
+        public int Month { get; set; }
+        public CancellationToken Token { get; set; }
+    }
+    // Д.З. Реализовать отмену потоков в "варианте 3" с комплексной передачей
+    // параметров в поток (детали в коде)
 }
